789e8c576917d5735afd485dadf874394eca9c11
jakotya <106250490+jakotya@users.noreply.github.com>
2022-07-16 22:53:20 +0200

    2nd full version

---
+++ script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+    $('.header__burger').click(function(event) {
+      $('.header__burger, .header__navbar, .header__navbar_active').toggleClass('active');
+      $('body').toggleClass('lock');
+    });
+  });
+
+function quiz() {
+    let name = prompt("What is your name?");
+    let country = prompt(
+      "Which country has the largest animation culture in the world?"
+    );
+    if (country === "japan") {
+      alert("Yes, " + name + "! That's right!");
+    } else {
+      alert("No, " + name + "! It's Japan)");
+    }
+    let email = prompt(
+      "Leave your email if you like my site or for additional quiz))"
+    );
+    alert("Great! I'll text you soon))");
+  }
+
+  let button = document.querySelector(".btn");
+  button.addEventListener("click", quiz);
+
